29d56c3a639a640ab77f78abf576134322f1b520
breezedoc <21306570@student.uwl.ac.uk>
2018-03-13 10:55:29 +0000

    version 0.0.2

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-# version 0.0.1
-==================================================================
+# version 0.0.2
+======================================
+Update:
 
-*   The project file contains a basic command-line processor which takes the user input
-    and breaks the string into a list of strings.
-    The list of symbols are then converted in order to be processed efficiently using conditionals.
+*   A decision table and lists of responses have been added to the main project file.
+*  Note that this version of the program will run just like the earlier version as some functions still need to be added to the code. This will be done in the upcomming updates.
 
-==================================================================
+* Added components:
+    -   Room 1
+    -   Corridor
+    -   Room 2A
+    -   Room 2B
--- main.rkt
+++ main.rkt
@@ -4,6 +4,31 @@
 (require srfi/13)
 (require srfi/48)
 
+
+(define responses
+'(
+  ;;Room 1
+(1 "You are in a room, you see a bag, a coffin and a door")
+(2 "You found a key. What do you do now?")
+(3 "The coffin is empty and has trace of a giant fingerprint.")
+  ;;Corridor
+(4 "You are in a corridor with two doors, one at your left and one at your right.")
+  ;; Room 2A
+(5 "It's a trap! you fell off in dark hole.")
+  ;; Room 2B'
+(6 "It's your lucky day! There is a weapon on the floor")
+(7 "You are now equiped. Watch this space!")
+ ))
+
+(define decisiontable
+'((1 ((open search bag) 2) ((open search coffin) 3) ((open search door) 4) ((look around)1))
+  
+(2 ((open search bag) 2) ((open search coffin) 3) ((open search door) 4) ((look around)1))
+(3 ((open search bag) 2) ((open search coffin) 3) ((open search door) 4) ((look around)1))
+(4 ((go left) 5) ((go right) 6))
+(6 ((pick) 7))))
+
+
 ;; Getting the user's input
 (define (command)
   (let* ((input (read-line))
